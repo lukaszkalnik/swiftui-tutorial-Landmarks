98a41b0cd076e6c0a05a23ffb36ed5b154445af7
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2024-04-05 18:59:15 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lukasz on 05.04.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
